dbcb6a60523d574e883c571c45a25e94781b762c
ConesaJM <javier07062000@gmail.com>
2026-03-08 20:09:25 +0100

    ejemplo de mail_config

---
+++ app/mail_config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'username' => 'correo@gmail.com',
+    'password' => 'password_de_aplicacion',
+    'from_email' => 'correo@gmail.com',
+    'from_name' => 'PharmaSphere',
+    'secure' => 'tls'
+];
